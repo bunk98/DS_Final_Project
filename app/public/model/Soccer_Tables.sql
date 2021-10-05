53531ac498c2bb42c7be4646efa1c51324e4ccdf
bunk98 <bunk98@gmail.com>
2021-10-04 23:00:43 -0400

    data inserts added

--- app/public/model/Soccer_Tables.sql
+++ app/public/model/Soccer_Tables.sql
@@ -28,3 +28,21 @@ FOREIGN KEY (refereeID) REFERENCES Referee(refereeID),
 gameID INT(6) NOT NULL,
 FOREIGN KEY (gameID) REFERENCES Game(gameID)
 );
+
+insert into Referee (firstname, lastname, grade, rating, dob) values ('Adda', 'McElwee', 'A', 69, '2018-12-09');
+insert into Referee (firstname, lastname, grade, rating, dob) values ('Katey', 'Abbis', 'B', 2, '2018-01-29');
+insert into Referee (firstname, lastname, grade, rating, dob) values ('Rubia', 'Waith', 'C', 56, '2020-10-26');
+insert into Referee (firstname, lastname, grade, rating, dob) values ('Jody', 'Pawlik', 'B', 60, '2021-01-30');
+insert into Referee (firstname, lastname, grade, rating, dob) values ('Tiffie', 'Guthrie', 'A', 3, '2019-01-02');
+
+insert into Game (field, game_name, game_time) values ('Field1', 'birds vs. dogs', '10:00:00');
+insert into Game (field, game_name, game_time) values ('Field2', 'dogs vs. cats', '10:00:00');
+insert into Game (field, game_name, game_time) values ('Field3', 'moose vs. dogs', '10:00:00');
+insert into Game (field, game_name, game_time) values ('Field1', 'walrus vs. cats', '10:00:00');
+insert into Game (field, game_name, game_time) values ('Feild2', 'walrus vs. birds', '10:00:00');
+
+insert into Ref_Game (rg_status, position, refereeID, gameID) values ('Assigned', 'Line judge', 1, 3);
+insert into Ref_Game (rg_status, position, refereeID, gameID) values ('Tenative', 'Goal line', 1, 2);
+insert into Ref_Game (rg_status, position, refereeID, gameID) values ('Accepted', 'Mid line', 2, 3);
+insert into Ref_Game (rg_status, position, refereeID, gameID) values ('Accepted', 'Goal line', 4, 1);
+insert into Ref_Game (rg_status, position, refereeID, gameID) values ('Assigned', 'Mid line', 5, 4);
