7a518996e7ae2299d563c77e076c3919eb5c85d4
asha0903 <57210527+asha0903@users.noreply.github.com>
2021-10-30 02:30:43 -0400

    EDITED REFEREE PAGE

--- app/public/api/referee/index.php
+++ app/public/api/referee/index.php
@@ -6,7 +6,7 @@ require 'class/DbConnection.php';
 $db = DbConnection::getConnection();
 
 // Step 2: Create & run the query
-$sql = 'SELECT * FROM Referee';
+$sql = 'SELECT * FROM referee';
 $vars = [];
 
 // if (isset($_GET['guid'])) {
@@ -18,10 +18,10 @@ $vars = [];
 $stmt = $db->prepare($sql);
 $stmt->execute($vars);
 
-$patients = $stmt->fetchAll();
+$referee = $stmt->fetchAll();
 
 // Step 3: Convert to JSON
-$json = json_encode($patients, JSON_PRETTY_PRINT);
+$json = json_encode($referee, JSON_PRETTY_PRINT);
 
 // Step 4: Output
 header('Content-Type: application/json');
--- app/public/js/referee.js
+++ app/public/js/referee.js
@@ -5,42 +5,130 @@ const Offer = {
         return {
 
             "referee" : [],
-            offerForm: {}
+            "offers": [],
+            "offerForm": {},
+            selectedOffer : null
 
         }
 
     },
+    
 
-    computed: {
-
-    },
+    computed: {},
 
     methods: {
+        prettyData(d) {
+            return dayjs(d)
+            .format('D MMM YYYY')
+        },  
         
 
         fetchRefereeData() {
             fetch('/api/referee/')
-
-            .then( response => response.json() )
-
-            .then( (responseJson) => {
-
-                console.log(responseJson);
-
-                this.referee = responseJson;
-
+            .then(response => response.json())
+            .then((parsedJson) => {
+                console.log(parsedJson);
+                this.referee = parsedJson
             })
-
-            .catch( (err) => {
-
-                console.error(err);
-
+            .catch( err => {
+                console.error(err)
             })
-        }
+        },
 
-    },
+        postOffer(evt) {
+            if (this.selectedOffer === null) {
+                this.postNewOffer(evt);
+            } else {
+                this.postEditOffer(evt);
+            }
+          },
 
-        
+        postNewOffer(evt) {
+            // this.offerForm.studentId = this.selectedStudent.id;        
+            // console.log("Posting:", this.offerForm);
+            //  alert("Posting!");
+    
+            fetch('api/referee/create.php', {
+                method:'POST',
+                body: JSON.stringify(this.offerForm),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.referee = json;
+                
+                // reset the form
+                this.resetOfferForm();
+
+              })
+              
+              .catch( err => {
+                alert("Please fill the requirements!");
+              });
+          },
+
+          postEditOffer(evt) {
+            // this.offerForm.studentId = this.selectedStudent.id;
+            this.offerForm.id = this.selectedOffer.id;
+    
+            // console.log("Updating!", this.offerForm);
+    
+            fetch('api/referee/update.php', {
+                method:'POST',
+                body: JSON.stringify(this.offerForm),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.referee = json;
+    
+                // reset the form
+                this.resetOfferForm();
+              });
+          },
+
+          postDeleteOffer(o) {
+            if (!confirm("Are you sure you want to delete the offer from "+o.id+"?")) {
+              return;
+            }
+            console.log("Delete!", o);
+    
+            fetch('api/referee/delete.php', {
+                method:'POST',
+                body: JSON.stringify(o),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.referee = json;
+    
+                // reset the form
+                this.resetOfferForm();
+              });
+          },
+          
+          selectOfferToEdit(o) {
+              this.selectedOffer = o;
+              this.offerForm = Object.assign({}, this.selectedOffer);
+          },
+
+          resetOfferForm() {
+              this.selectedOffer = null;
+              this.offerForm = {};
+          }
+      },
 
 
     created(){
@@ -51,4 +139,7 @@ const Offer = {
 
 }
 
+
+
 Vue.createApp(Offer).mount('#offerApp');
+
